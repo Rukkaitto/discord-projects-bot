85c075ace3d83fb563c0422ac5365ecfc2f3d1b0
Lucas Goudin <goudinlucas@gmail.com>
2021-04-05 22:05:56 +0200

    Refactored api and disabled mongo logs

---
+++ api/src/endpoints.js
@@ -0,0 +1,72 @@
+const { Server } = require("./db");
+
+const getServers = async (req, res) => {
+  const servers = await Server.find({});
+  res.json(servers);
+};
+
+const getServer = async (req, res) => {
+  const { serverId } = req.params;
+  const server = await Server.findById(serverId);
+  res.json(server);
+};
+
+const postServer = async (req, res) => {
+  const { id, name } = req.body;
+  server = new Server({
+    _id: id,
+    name: name,
+  });
+  const result = await server.save();
+  res.json(result);
+};
+
+const postProject = async (req, res) => {
+  const { serverId } = req.params;
+  const { title, id, username, avatar } = req.body;
+  const server = await Server.findById(serverId);
+  if (server) {
+    server.projects.push({
+      title: title,
+      members: [
+        {
+          _id: id,
+          username: username,
+          avatar: avatar,
+        },
+      ],
+    });
+    const result = await server.save();
+    if (result) {
+      res.json({ response: "Successfully created new project." });
+    } else {
+      res.json({ response: "Could not create project." });
+    }
+  }
+};
+
+const postMember = async (req, res) => {
+  const { serverId, projectId } = req.params;
+  const { id, username, avatar } = req.body;
+  const server = await Server.findById(serverId);
+  const project = await server.projects.id(projectId);
+  project.members.push({
+    _id: id,
+    username: username,
+    avatar: avatar,
+  });
+  const result = await server.save();
+  if (result) {
+    res.json({ response: "Successfully added member to project." });
+  } else {
+    res.json({ response: "Could not add member to project." });
+  }
+};
+
+module.exports = {
+  getServers,
+  getServer,
+  postServer,
+  postProject,
+  postMember,
+};
--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,14 @@
 require("dotenv").config();
 const express = require("express");
+const { db, Server } = require("./db");
+const {
+  getServers,
+  getServer,
+  postServer,
+  postProject,
+  postMember,
+} = require("./endpoints");
 const api = express();
-const { db, Server, Project, User } = require("./db");
 
 api.use(express.json());
 
@@ -10,70 +17,15 @@ const port = 3000;
 db.on("error", console.error.bind(console, "connection error:"));
 
 db.once("open", () => {
-  api.get("/servers", async (req, res) => {
-    const servers = await Server.find({});
-    if (servers) {
-      res.json(servers);
-    }
-  });
+  api.get("/servers", getServers);
 
-  api.get("/:serverId", async (req, res) => {
-    const { serverId } = req.params;
-    const server = await Server.findById(serverId);
-    res.json(server);
-  });
+  api.get("/:serverId", getServer);
 
-  api.post("/servers", async (req, res) => {
-    const { id, name } = req.body;
-    server = new Server({
-      _id: id,
-      name: name,
-    });
-    const result = await server.save();
-    res.json(result);
-  });
+  api.post("/servers", postServer);
 
-  api.post("/:serverId/projects", async (req, res) => {
-    const { serverId } = req.params;
-    const { title, id, username, avatar } = req.body;
-    const server = await Server.findById(serverId);
-    if (server) {
-      server.projects.push({
-        title: title,
-        members: [
-          {
-            _id: id,
-            username: username,
-            avatar: avatar,
-          },
-        ],
-      });
-      const result = await server.save();
-      if (result) {
-        res.json({ response: "Successfully created new project." });
-      } else {
-        res.json({ response: "Could not create project." });
-      }
-    }
-  });
+  api.post("/:serverId/projects", postProject);
 
-  api.post("/:serverId/:projectId/members", async (req, res) => {
-    const { serverId, projectId } = req.params;
-    const { id, username, avatar } = req.body;
-    const server = await Server.findById(serverId);
-    const project = await server.projects.id(projectId);
-    project.members.push({
-      _id: id,
-      username: username,
-      avatar: avatar,
-    });
-    const result = await server.save();
-    if (result) {
-      res.json({ response: "Successfully added member to project." });
-    } else {
-      res.json({ response: "Could not add member to project." });
-    }
-  });
+  api.post("/:serverId/:projectId/members", postMember);
 
   api.listen(port, () => {
     console.log(`Listening on port ${port}`);
